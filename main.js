2ba9a9d70d9d84f26f0f7d9e9a5b867adb5b2f4a
jige <1527087751@qq.com>
2025-01-23 17:23:41 -0500

    change the version

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         D2L Quiz Scraper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  抓取 D2L Quiz 中的选择题题目与选项
 // @author       William
 // @match https://avenue.cllmcmaster.ca/d2l/lms/quizzing/user/attempt/*
